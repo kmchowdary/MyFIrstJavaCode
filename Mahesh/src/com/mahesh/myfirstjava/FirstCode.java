906bf077d35f7dfdd7bfc0752e226ad6e46423a0
Ghost <Ghost@DESKTOP-GBPQDIV>
2020-05-09 22:41:58 +0530

    This is my new commint

---
+++ Mahesh/src/com/mahesh/myfirstjava/FirstCode.java
@@ -0,0 +1,10 @@
+package com.mahesh.myfirstjava;
+
+public class FirstCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("This Is My First Program");
+	}
+
+}
